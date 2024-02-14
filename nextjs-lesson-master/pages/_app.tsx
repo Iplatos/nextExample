8d46584e83ffbadc61221f91a1a00206a05a6d71
iplatos <platonoff9168@gmail.com>
2024-02-14 11:14:49 +0300

    feat: add Layout

--- nextjs-lesson-master/pages/_app.tsx
+++ nextjs-lesson-master/pages/_app.tsx
@@ -3,6 +3,9 @@ import {ReactElement, ReactNode, useState} from 'react';
 import {NextPage} from 'next';
 import {QueryClient} from "@tanstack/query-core";
 import {QueryClientProvider,Hydrate} from "@tanstack/react-query";
+import {useLoader} from "../assets/hooks/useLoader";
+import "../styles/nprogress.css"
+
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,7 +18,7 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const [queryClient] = useState(()=> new QueryClient())
   const getLayout = Component.getLayout ?? ((page) => page);
-
+useLoader()
   return getLayout(<QueryClientProvider client={queryClient}>
     <Hydrate state={pageProps.dehydrateState} ><Component {...pageProps} /></Hydrate>
   </QueryClientProvider>);
