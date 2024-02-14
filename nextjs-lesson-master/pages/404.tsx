7175ba1836de4c86fce3033f247780474a6fed53
iplatos <platonoff9168@gmail.com>
2024-02-14 14:41:37 +0300

    feat: status

---
+++ nextjs-lesson-master/pages/404.tsx
@@ -0,0 +1,6 @@
+import {PageWrapper} from "components/PageWrapper/PageWrapper";
+
+const NotFound = () => {
+ return <PageWrapper>404 not found</PageWrapper>
+}
+NotFound.getLayaut
--- nextjs-lesson-master/pages/characters/[id].tsx
+++ nextjs-lesson-master/pages/characters/[id].tsx
@@ -4,6 +4,7 @@ import {CharacterCard} from "../../components/Card/CharacterCard/CharacterCard";
 import {CharacterType} from "../../assets/api/rick-and-morty-api";
 import {GetStaticProps} from "next";
 import {useRouter} from "next/router";
+import styled from "styled-components";
 
 export const getStaticPaths = async () => {
   const {results} = await API.rickAndMorty.getCharacters()
@@ -20,7 +21,6 @@ return {
  export const getStaticProps: GetStaticProps = async ({params})=>{
     const {id} = params || {}
     const character = await API.rickAndMorty.getCharacter(id as string)
-    console.log(character)
     if(!character){
       return {
         notFound:true
@@ -42,9 +42,10 @@ const {character} = props
 
   const router = useRouter()
   if (router.isFallback) return <h1>loading</h1>
-
   return (
     <div>
+      <Button onClick={()=>router.push("/characters")}>go to characters</Button>
+      <IdText>id:{router.query.id}</IdText>
       <CharacterCard  character={character}/>
     </div>
 
@@ -52,3 +53,10 @@ const {character} = props
 }
 Character.getLayout = getLayout
 export default Character
+
+const IdText = styled.div`
+font-size:38px
+`
+const Button = styled.button`
+background-color: #fa52d3;
+`
