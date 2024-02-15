5d72547d76ed477f1be850c77c001992cc84441a
iplatos <platonoff9168@gmail.com>
2024-02-15 10:51:55 +0300

    feat: add page 404

--- nextjs-lesson-master/pages/404.tsx
+++ nextjs-lesson-master/pages/404.tsx
@@ -1,6 +1,8 @@
 import {PageWrapper} from "components/PageWrapper/PageWrapper";
+import {getLayout} from "components/Layout/BaseLayout/BaseLayout";
 
 const NotFound = () => {
- return <PageWrapper>404 not found</PageWrapper>
+ return <PageWrapper>404 not found dubu dubi</PageWrapper>
 }
-NotFound.getLayaut
+NotFound.getLayout = getLayout
+export default NotFound
