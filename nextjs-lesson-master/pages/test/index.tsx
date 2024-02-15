5ed72b3af25cbe7f612962367c7f15ee2727978c
iplatos <platonoff9168@gmail.com>
2024-02-15 13:10:34 +0300

    feat: temp test

--- nextjs-lesson-master/pages/test/index.tsx
+++ nextjs-lesson-master/pages/test/index.tsx
@@ -1,35 +1,45 @@
-
+// @ts-nocheck
 import {API} from "../../assets/api/api";
 import {CharacterType, ResponseType} from "../../assets/api/rick-and-morty-api";
 import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
 import {CharacterCard} from "../../components/Card/CharacterCard/CharacterCard";
 import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout";
+import * as path from "path";
+import * as fs from "fs";
 
 
 export const getStaticProps = async ()=>{
-  const characters = await API.rickAndMorty.getCharacters()
+const getParseData = async ()=>{
+  const filePath = path.join(process.cwd(), "public", "staticData")
+  try{
+const jsonData = await fs.readFile
+    return JSON.parse(jsonData.toString())
+  } catch (err){
+return {title:"no Title"}
+  }
+}
+  const title = ""
   return {
     props:{
-      characters
+      title
     },
-    revalidate:60
   }
 }
 
 type PropsType = {
-  characters: ResponseType<CharacterType>
+  title: string
 }
 
-const Characters = (props:PropsType) => {
-const {characters} = props
-const characterList = characters.results.map(ch => <CharacterCard key={ch.id} character={ch}/>)
+const Test = (props:PropsType) => {
+  const {title} = props
 
 
   return (
     <PageWrapper>
-      {characterList}
+      <>asd</>
+      {title}
     </PageWrapper>
   );
 };
-Characters.getLayout = getLayout
-export default Characters;
+Test.getLayout = getLayout
+export default Test;
