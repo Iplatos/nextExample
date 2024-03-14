f862afaabb9b3e444ee114eaa7e6b8297764c671
iplatos <platonoff9168@gmail.com>
2024-03-14 12:19:45 +0300

    add status

--- nextjs-lesson-master/components/Card/CharacterCard/CharacterCard.tsx
+++ nextjs-lesson-master/components/Card/CharacterCard/CharacterCard.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import aliveStatus from "public/statuses/alive.png";
 import deadStatus from "public/statuses/dead.png";
 import unknownStatus from "public/statuses/unknown.png";
-import {CharacterType} from '../../../assets/api/rick-and-morty-api';
+import {CharacterType} from 'assets/api/rick-and-morty-api';
 
 const statusImages = {
   Alive: aliveStatus,
@@ -19,9 +19,11 @@ type PropsType = {
 
 export const CharacterCard = (props: PropsType) => {
   const { id, name, image, status } = props.character;
-
   return (
     <Card name={name}>
+
+      <Image src={statusImages[status]} alt={""} width={30} height={30}/>
+
       <Link href={`/characters/${id}`}>
         <ImageBlock src={image} alt={name} width={300} height={300} priority />
       </Link>
--- nextjs-lesson-master/pages/characters/index.tsx
+++ nextjs-lesson-master/pages/characters/index.tsx
@@ -1,9 +1,6 @@
-import {CharacterType, RickAndMortyApi,ResponseType} from "assets/api/rick-and-morty-api";
+import {CharacterType, ResponseType} from "assets/api/rick-and-morty-api";
 import {API} from "assets/api/api";
 import {PageWrapper} from "components/PageWrapper/PageWrapper";
-
-import Image from "next/image";
-import {Header} from "components/Header/Header";
 import {CharacterCard} from "components/Card/CharacterCard/CharacterCard";
 import {getLayout} from "components/Layout/BaseLayout/BaseLayout";
 
@@ -20,7 +17,7 @@ const Characters = (props: PropsType) => {
   const {characters} = props
   return (
     <PageWrapper>
-      <>{characters.results.map(character=><CharacterCard key={character.id} character={character}  />)}</>
+      <>{characters.results.map(character=><CharacterCard key={character.id}  character={character}  />)}</>
     </PageWrapper>
 
   )
